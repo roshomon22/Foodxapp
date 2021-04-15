9c1e582a68e32581cf8ee8162b51bdd2b9819730
Roshan Ravichandran <54618288+roshomon22@users.noreply.github.com>
2021-04-15 07:13:43 +0300

    register activity added

---
+++ app/src/main/java/com/example/foodx/ShareFoodActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShareFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_food);
+    }
+}
