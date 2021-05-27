364ba28806fb435616d7172736be06e6dc1cc463
Roshan Ravichandran <54618288+roshomon22@users.noreply.github.com>
2021-05-27 12:51:51 +0300

    register activity added

---
+++ app/src/main/java/com/example/foodx/MainViewFood.java
@@ -0,0 +1,14 @@
+package com.example.foodx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainViewFood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view_food);
+    }
+}
--- app/src/main/java/com/example/foodx/MyAdapter.java
+++ app/src/main/java/com/example/foodx/MyAdapter.java
@@ -24,7 +24,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.share_food_holder,parent,false);
+        View v = LayoutInflater.from(context).inflate(R.layout.activity_main_view_food,parent,false);
         return new MyViewHolder(v);
 
 
