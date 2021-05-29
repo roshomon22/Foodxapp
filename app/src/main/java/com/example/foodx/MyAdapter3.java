dbf646d6128acf3577c1cffa90d247e4617ace8a
Roshan Ravichandran <54618288+roshomon22@users.noreply.github.com>
2021-05-29 20:30:59 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/foodx/ViewActivity.java

---
+++ app/src/main/java/com/example/foodx/MyAdapter3.java
@@ -0,0 +1,80 @@
+package com.example.foodx;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+public class MyAdapter3 extends RecyclerView.Adapter<MyAdapter3.YrViewHolder>{
+    ArrayList<Incoming> pList3;
+
+
+    Context context3;
+
+
+    public MyAdapter3(Context context3, ArrayList<Incoming> pList3){
+        this.pList3=pList3;
+        this.context3=context3;
+
+    }
+
+
+    @NonNull
+    @Override
+    public YrViewHolder onCreateViewHolder(@NonNull ViewGroup parent3, int viewType) {
+        View t = LayoutInflater.from(context3).inflate(R.layout.view_incom_req,parent3,false);
+
+
+        return new YrViewHolder(t);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull YrViewHolder holder3, int position3) {
+        String UserID =FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        Incoming Incoming =pList3.get(position3);
+        holder3.Item.setText(Incoming.getItem());
+        holder3.location.setText(Incoming.getLocation());
+        holder3.ReqUserID.setText(Incoming.getReqUserID());
+
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return pList3.size();
+    }
+
+    public class YrViewHolder extends RecyclerView.ViewHolder {
+
+        TextView Item,location, ReqUserID;
+
+
+
+        public YrViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            Item= itemView.findViewById(R.id.user2);
+            location =itemView.findViewById(R.id.food_14);
+            ReqUserID = itemView.findViewById(R.id.userid3);
+
+
+
+        }
+
+    }
+
+}
